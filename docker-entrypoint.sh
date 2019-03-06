01fb48612802b296ba7470d39d5943e633b9279b
ajalinux <47218439+ajalinux@users.noreply.github.com>
2019-03-06 12:28:05 +0530

    Add files via upload

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TZ=${TZ:-Etc/UTC}
+CRON_TIMING=${CRON_TIMING:-'* * * * *'}
+APACHE_RUN_USER=${APACHE_RUN_USER:-'www-data'}
+APACHE_RUN_GROUP=${APACHE_RUN_GROUP:-'www-data'}
+
+echo --------------------------------------------------
+echo "Setting up Timzone: \"${TZ}\""
+echo --------------------------------------------------
+echo $TZ | tee /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
+
+set -ex
+
+# Reload Cron
+echo "${CRON_TIMING} /usr/bin/php5 /var/www/html/front/cron.php &>/dev/null" | crontab -u ${APACHE_RUN_USER} -
+/etc/init.d/cron reload
+/etc/init.d/cron restart
+
+exec "$@"
